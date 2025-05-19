e306897b954d3ceb7507ac1162dcc28bc3619f2d
Badri Vishal Singh <123944508+badrisinghoo7@users.noreply.github.com>
2025-05-19 22:44:37 +0530

    Update index.js
    All js code is done

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
 
+//1. FlattenArray
+function flatten(arr){
+    let stack = [...arr];
+    let res = [];
+    
+    while(stack.length){
+        let curr = stack.pop();
+        
+        if(Array.isArray(curr)){
+            stack.push(...curr)
+        }
+        else{
+            res.push(curr)
+        }
+    }
+    return res.reverse()
+}
+
+console.log(flatten([1,2,3,[4,5,[6,7]]]))
+
+//2.
+
+
+function groupBy(fruits) {
+    let prod = {};
+
+    for (let i = 0; i < fruits.length; i++) {
+        const el = fruits[i];
+        const type = el.type;
+
+        if (!prod[type]) {
+            prod[type] = [];
+        }
+
+        prod[type].push(el);
+    }
+
+    return prod;
+}
+
+
+//3.
+
+ function deepClone(obj) {
+    if ( typeof obj !== 'object') {
+      return obj;
+    }
+    
+    if (Array.isArray(obj)) {
+      return obj.map(el => this.deepClone(el));
+    }
+    
+    const obj2 = {};
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        obj2[key] = this.deepClone(obj[key]);
+      }
+    }
+    
+    return obj2;
+  },
+
+
+
+
+//4.
+
+Array.prototype.myMap = function(cb) {
+  let result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    result.push(cb(this[i], i, this));
+  }
+
+  return result;
+};
+
+//5. 
+Array.prototype.myReduce = function(cb, init) {
+  let acc = init;
+  let start = 0;
+
+  if (acc === undefined) {
+    acc = this[0];
+    start = 1;
+  }
+
+  for (let i = start; i < this.length; i++) {
+    acc = cb(acc, this[i], i, this);
+  }
+
+  return acc;
+};
+
+console.log([1,2,3,4,5].myReduce((acc,el)=>{
+    return acc+el
+},0))
